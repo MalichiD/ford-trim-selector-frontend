ca735fc867192e104269e8eb2bfba51b43350dc7
Malichi Hill <hillmalichi@outlook.com>
2024-12-01 13:58:53 -0500

    Update axiosConfig.js with API URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://3b28-35-151-153-4.ngrok-free.app',
+    headers: {"ngrok-skip-browser-warning": "true"}
 });
